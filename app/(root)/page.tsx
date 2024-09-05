3b0110a2870565a016b13c38c6270cc803ade3f6
Kai Williams <kailuwilliams@gmail.com>
2024-09-04 18:18:39 -0700

    add header box component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,23 @@
+import HeaderBox from '@/components/ui/HeaderBox'
 import React from 'react'
 
 const Home = () => {
+  const loggedIn = { firstName: 'Kai' }
+
   return (
-    <div>Home</div>
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || 'Guest'}
+            subtext="Access and manage your account
+            and transactions efficiently."
+          />
+        </header>
+      </div>
+    </section>
   )
 }
 
