74530be14c646ba70484fcca52c6b1f4184f39ba
Kai Williams <kailuwilliams@gmail.com>
2024-09-07 15:00:03 -0700

    fix cursor pointer class

--- components/ui/MobileNav.tsx
+++ components/ui/MobileNav.tsx
@@ -31,7 +31,7 @@ const MobileNav = ( { user }: MobileNavProps) => {
         </SheetTrigger>
         <SheetContent side="left" className="border-none bg-white">
           <nav className="flex flex-col gap-4">
-            <Link href="/" className="flex cursor items-center gap-1 px-4">
+            <Link href="/" className="flex cursor-pointer items-center gap-1 px-4">
               <Image
                 src="/icons/logo.svg"
                 width={34}
