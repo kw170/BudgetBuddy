a77817b7cc5960d060a8baa437263f1dc29b7696
Kai Williams <kailuwilliams@gmail.com>
2024-09-08 23:45:27 -0700

    dynamically retrieve name for welcome

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,9 +2,10 @@ import HeaderBox from '@/components/ui/HeaderBox'
 import TotalBalanceBox from '@/components/ui/TotalBalanceBox'
 import React from 'react'
 import RightSidebar from '@/components/ui/RightSidebar'
+import { getLoggedInUser } from '@/lib/actions/user.actions'
 
-const Home = () => {
-  const loggedIn = { firstName: 'Kai', lastName: 'Williams', email: 'kailuwilliams@gmail.com'}
+const Home = async () => {
+  const loggedIn = await getLoggedInUser()
 
   return (
     <section className="home">
@@ -13,7 +14,7 @@ const Home = () => {
           <HeaderBox
             type="greeting"
             title="Welcome"
-            user={loggedIn?.firstName || 'Guest'}
+            user={loggedIn?.name || 'Guest'}
             subtext="Access and manage your account
             and transactions efficiently."
           />
