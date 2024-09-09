dd52138f05de8077df755f148884d68985b6fa4d
Kai Williams <kailuwilliams@gmail.com>
2024-09-08 20:07:44 -0700

    add city field

--- lib/utils.ts
+++ lib/utils.ts
@@ -195,11 +195,12 @@ export const getTransactionStatus = (date: Date) => {
   return date > twoDaysAgo ? "Processing" : "Success";
 };
 
-export const authFormSchema = (type) => z.object({
+export const authFormSchema = (type: string) => z.object({
   // sign up
   firstName: type === 'sign-in'? z.string().optional(): z.string().min(3),
   lastName: type === 'sign-in'? z.string().optional(): z.string().min(3),
   address1: type === 'sign-in'? z.string().optional(): z.string().min(3).max(50),
+  city: type === 'sign-in'? z.string().optional(): z.string().min(2).max(50),
   state: type === 'sign-in'? z.string().optional(): z.string().min(2).max(2),
   postalCode: type === 'sign-in'? z.string().optional(): z.string().min(3).max(6),
   dateOfBirth: type === 'sign-in'? z.string().optional(): z.string().min(10).max(10),
